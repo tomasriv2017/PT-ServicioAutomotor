09539b4bbcd2b8fdb9014404b2e6b756b4ee1a3b
tomasriv2017 <tomyriv@hotmail.com>
2023-05-03 23:01:19 -0300

    Initial commit

---
+++ src/main/java/com/PruebaTecnica/ServicioAutomotor/ServicioAutomotorApplication.java
@@ -0,0 +1,13 @@
+package com.PruebaTecnica.ServicioAutomotor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioAutomotorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioAutomotorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PruebaTecnica/ServicioAutomotor/ServicioAutomotorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PruebaTecnica.ServicioAutomotor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioAutomotorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
